604d3cdbb9c97ff820812ab1b5778fe0b421fc84
Nitin Jaiman <jaimann@amazon.com>
2019-08-04 21:41:42 +0530

    Added thread.sleep

--- src/main/java/com/trading/flarebot/experimental/ExchangeTicker.java
+++ src/main/java/com/trading/flarebot/experimental/ExchangeTicker.java
@@ -2,7 +2,6 @@ package com.trading.flarebot.experimental;
 
 import com.trading.flarebot.unifiedtradingview.ExchangeClientFactory;
 import com.trading.flarebot.unifiedtradingview.UnifiedTradingViewClient;
-import com.trading.flarebot.unifiedtradingview.config.SupportedExchange;
 import com.trading.flarebot.unifiedtradingview.constant.ExchangeConstant;
 import com.trading.flarebot.unifiedtradingview.constant.SymbolConstant;
 import com.trading.flarebot.unifiedtradingview.model.ClientMetadata;
@@ -11,7 +10,7 @@ public class ExchangeTicker implements Runnable {
     private String exchange;
     private String symbol;
 
-    public ExchangeTicker(String exchange, String symbol){
+    public ExchangeTicker(String exchange, String symbol) {
         this.exchange = exchange;
         this.symbol = symbol;
     }
@@ -20,11 +19,17 @@ public class ExchangeTicker implements Runnable {
     public void run() {
         ClientMetadata clientMetadata = new ClientMetadata();
         clientMetadata.setExchangeName(ExchangeConstant.BINANCE);
-        UnifiedTradingViewClient unifiedTradingViewClient = ExchangeClientFactory.getUnifiedTradingViewClient(clientMetadata);
+        UnifiedTradingViewClient unifiedTradingViewClient = ExchangeClientFactory
+                .getUnifiedTradingViewClient(clientMetadata);
 
-        while(true){
-            System.out.println("ServerTime: "+unifiedTradingViewClient.getServerTime());
-            System.out.println("Price: "+unifiedTradingViewClient.getPrice(SymbolConstant.BTCUSDT));
+        while (true) {
+            System.out.println("ServerTime: " + unifiedTradingViewClient.getServerTime());
+            System.out.println("Price: " + unifiedTradingViewClient.getPrice(SymbolConstant.BTCUSDT));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
