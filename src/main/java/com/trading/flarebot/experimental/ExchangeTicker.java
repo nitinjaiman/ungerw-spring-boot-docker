ddb81aa59a15cfa204692bccfe773c98cd0b1f46
Nitin Jaiman <jaimann@amazon.com>
2019-08-04 21:02:36 +0530

    experimental commit to test UnifiedTradingView

---
+++ src/main/java/com/trading/flarebot/experimental/ExchangeTicker.java
@@ -0,0 +1,31 @@
+package com.trading.flarebot.experimental;
+
+import com.trading.flarebot.unifiedtradingview.ExchangeClientFactory;
+import com.trading.flarebot.unifiedtradingview.UnifiedTradingViewClient;
+import com.trading.flarebot.unifiedtradingview.config.SupportedExchange;
+import com.trading.flarebot.unifiedtradingview.constant.ExchangeConstant;
+import com.trading.flarebot.unifiedtradingview.constant.SymbolConstant;
+import com.trading.flarebot.unifiedtradingview.model.ClientMetadata;
+
+public class ExchangeTicker implements Runnable {
+    private String exchange;
+    private String symbol;
+
+    public ExchangeTicker(String exchange, String symbol){
+        this.exchange = exchange;
+        this.symbol = symbol;
+    }
+
+    @Override
+    public void run() {
+        ClientMetadata clientMetadata = new ClientMetadata();
+        clientMetadata.setExchangeName(ExchangeConstant.BINANCE);
+        UnifiedTradingViewClient unifiedTradingViewClient = ExchangeClientFactory.getUnifiedTradingViewClient(clientMetadata);
+
+        while(true){
+            System.out.println("ServerTime: "+unifiedTradingViewClient.getServerTime());
+            System.out.println("Price: "+unifiedTradingViewClient.getPrice(SymbolConstant.BTCUSDT));
+        }
+
+    }
+}
--- src/main/java/com/trading/flarebot/service/HelloWorldService.java
+++ src/main/java/com/trading/flarebot/service/HelloWorldService.java
@@ -1,5 +1,8 @@
 package com.trading.flarebot.service;
 
+import com.trading.flarebot.experimental.ExchangeTicker;
+import com.trading.flarebot.unifiedtradingview.constant.ExchangeConstant;
+import com.trading.flarebot.unifiedtradingview.constant.SymbolConstant;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -17,6 +20,8 @@ public class HelloWorldService {
     @Path("/hello")
     public Response test() {
         System.out.println("logged");
+        Thread thread = new Thread(new ExchangeTicker(ExchangeConstant.BINANCE, SymbolConstant.BTCUSDT));
+        thread.start();
         return Response.status(200).entity("Hello from Nitin jaiman ").build();
     }
 
