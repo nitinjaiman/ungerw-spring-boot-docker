bddbcf16e43887ce6c9cbde0baa6117c723300bd
Nitin Jaiman <jaimann@amazon.com>
2019-08-10 21:41:44 +0530

    Added log4j2

--- src/main/java/com/trading/flarebot/service/HelloWorldService.java
+++ src/main/java/com/trading/flarebot/service/HelloWorldService.java
@@ -4,6 +4,7 @@ import com.trading.flarebot.experimental.ExchangeTicker;
 import com.trading.flarebot.experimental.TickerSingleton;
 import com.trading.flarebot.unifiedtradingview.constant.ExchangeConstant;
 import com.trading.flarebot.unifiedtradingview.constant.SymbolConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -15,12 +16,17 @@ import javax.ws.rs.core.Response;
  */
 @Component
 @Path("/spring-docker/")
+@Slf4j
 public class HelloWorldService {
 
     @GET
     @Path("/hello")
     public Response test() {
         System.out.println("logged");
+        log.info("Logged as info");
+        log.warn("Logged as warn");
+        log.error("Logged as error");
+        log.debug("Logged as debug");
         Thread thread = new Thread(new ExchangeTicker(ExchangeConstant.BINANCE, SymbolConstant.BTCUSDT));
         thread.start();
         return Response.status(200).entity("Hello from Nitin jaiman "+ TickerSingleton.price).build();
